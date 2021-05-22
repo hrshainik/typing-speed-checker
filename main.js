8008bea5ac86624ae8d252f22be68393a51a4ff2
hrshainik <hrsshainik@gmail.com>
2021-05-22 19:13:24 +0600

    Add basic functionality

--- main.js
+++ main.js
@@ -1,28 +1,82 @@
 // Get all dom element
-const providedText = document.querySelector(".text p");
+const providedText = document.querySelector(".text p").innerText;
 const userText = document.querySelector("#text-area");
 const wpmText = document.querySelector("#wpm");
 const timer = document.querySelector(".timer");
 
-console.log(providedText);
-console.log(userText);
-console.log(wpmText);
-console.log(timer);
+let millisec = 0;
+let sec = 0;
+let min = 0;
+let interval;
 
 // Clock functionality
+const runTimer = () => {
+  millisec = parseInt(millisec);
+  sec = parseInt(sec);
+  min = parseInt(min);
+
+  millisec = millisec + 1;
+
+  if (millisec == 100) {
+    sec = sec + 1;
+    millisec = 0;
+  }
+  if (sec == 60) {
+    min = min + 1;
+    sec = 0;
+    millisec = 0;
+  }
+
+  if (millisec < 10 || millisec == 0) {
+    millisec = "0" + millisec;
+  }
+  if (sec < 10 || sec == 0) {
+    sec = "0" + sec;
+  }
+  if (min < 10 || min == 0) {
+    min = "0" + min;
+  }
+
+  timer.innerHTML = `${min}:${sec}:${millisec}`;
+};
 
 // Start the timer
 const start = () => {
   let userTextLength = userText.value.length;
-  console.log(userTextLength);
+  if (!userTextLength) {
+    interval = setInterval(runTimer, 10);
+  }
 };
 
 // Calculate wpm functionality
+const calculateWpm = (str) => {
+  const totalTime = (min * 60 + sec) / 60;
+  str = str.replace(/(^\s*)|(\s*$)/gi, "");
+  str = str.replace(/[ ]{2,}/gi, " ");
+  str = str.replace(/\n /, "\n");
+  const totalWords = str.split(" ").length;
+  const wpm = totalWords / totalTime;
+  console.log(wpm);
+  wpmText.textContent = wpm;
+};
 
 // Match the text with the provided text
 const matchText = () => {
   let userTextEntered = userText.value;
-  console.log(userTextEntered);
+  let originTextMatch = providedText.substring(0, userTextEntered.length);
+  console.log(originTextMatch);
+
+  if (userTextEntered === providedText) {
+    userText.style.borderColor = "#50e2d6";
+    clearInterval(interval);
+    calculateWpm(providedText);
+  } else {
+    if (userTextEntered === originTextMatch) {
+      userText.style.borderColor = "#fff";
+    } else {
+      userText.style.borderColor = "#f30000";
+    }
+  }
 };
 
 // Event listeners
